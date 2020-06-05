88f6c3d44d29baa53720cdd246133f6c5cdd7435
unknown <felipevilla@vinixcode.com>
2020-06-05 16:10:07 -0500

    Scripts

---
+++ script.js
@@ -0,0 +1,126 @@
+window.sr = ScrollReveal();
+sr.reveal(".scrollreveal-ourwork", {
+  duration: 3000,
+  origin: "left",
+  distance: "300px",
+});
+
+sr.reveal(".vinix-carousel", {
+  duration: 3000,
+  origin: "rigth",
+  distance: "300px",
+});
+
+sr.reveal(".video-services", {
+  duration: 3000,
+  origin: "left",
+  distance: "300px",
+});
+
+sr.reveal(".img-service", {
+  duration: 4000,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(".scrollreveal-services", {
+  duration: 3000,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".text-vinixcode", {
+  duration: 3000,
+  origin: "top",
+  distance: "300px",
+});
+
+sr.reveal(".we-are-experts-title", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(".we-are-experts", {
+  duration: 3000,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".video-mission", {
+  duration: 3000,
+  origin: "left",
+  distance: "300px",
+});
+
+sr.reveal(".all-text", {
+  duration: 3000,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".title-aboutus", {
+  duration: 3000,
+  origin: "top",
+  distance: "300px",
+});
+
+sr.reveal(".text-about-us", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(".text-main-team", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(".text-our-people", {
+  duration: 3000,
+  origin: "top",
+  distance: "300px",
+});
+
+sr.reveal(".juan-garcia-team", {
+  duration: 3000,
+  origin: "top",
+  distance: "300px",
+});
+
+sr.reveal(".pablo-pulgarin-team", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(".felipe-villa-team", {
+  duration: 3000,
+  origin: "top",
+  distance: "300px",
+});
+
+sr.reveal(".katheryne-castro-team", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "300px",
+});
+
+sr.reveal(".carolina-herrera-team", {
+  duration: 3000,
+  origin: "top",
+  distance: "300px",
+});
+
+sr.reveal(".names-profiles", {
+  duration: 3000,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".team-position", {
+  duration: 3000,
+  origin: "left",
+  distance: "300px",
+});
